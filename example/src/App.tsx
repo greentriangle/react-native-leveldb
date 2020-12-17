d8734414b33f484a1de81bf800b5c4216510ebd9
Chris Savvopoulos <savvopoulos@gmail.com>
2020-12-17 15:44:28 +0100

    chore: run yarn

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Leveldb from 'react-native-leveldb';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
